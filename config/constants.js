28fbc97ec31439cabc7a61288b4a81d3f896db18
Karan Goel <karan@goel.im>
2014-03-22 09:47:17 -0700

    add api files

---
+++ config/constants.js
@@ -0,0 +1,13 @@
+/*
+    Holds app-wide constants.
+    Usage: 
+        var Constants = require(./config/Constants);
+        console.log(Constants.APP_NAME);
+*/
+
+var Constants = {
+    // The name of the app
+    APP_NAME: "SPA"
+};
+
+module.exports = Constants;
